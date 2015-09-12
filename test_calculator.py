41451d60edc69dc7b517ac69b518fc2d3e4f9ca2
Indrek Ots <ots.indrek@gmail.com>
2015-09-12 11:26:20 +0300

    add additional testcases

--- test_calculator.py
+++ test_calculator.py
@@ -11,5 +11,13 @@ class TestCalculator(unittest.TestCase):
       calc = Calculator()
       self.assertEqual(calc.calculate("4-1"), 3, "Should equal 3")
 
+  def test_add_and_subtract(self):
+      calc = Calculator()
+      self.assertEqual(calc.calculate("4+3-1"), 6, "Should equal 6")
+
+  def test_calculate_with_spaces(self):
+      calc = Calculator()
+      self.assertEqual(calc.calculate("4 - 3   +1"), 2, "Should equal 2")
+
 if __name__ == '__main__':
     unittest.main()
