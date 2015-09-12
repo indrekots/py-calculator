e92aed618310370fc48e5ac67c4f279ef152a500
Indrek Ots <ots.indrek@gmail.com>
2015-09-12 11:23:25 +0300

    rename test class

--- test_calculator.py
+++ test_calculator.py
@@ -1,11 +1,15 @@
 import unittest
 from calculator import Calculator
 
-class CalculatorTest(unittest.TestCase):
+class TestCalculator(unittest.TestCase):
 
   def test_add_two_numbers(self):
       calc = Calculator()
       self.assertEqual(calc.calculate("2+2"), 4, "Should equal 4")
+
+  def test_subtract(self):
+      calc = Calculator()
+      self.assertEqual(calc.calculate("4-1"), 3, "Should equal 3")
 
 if __name__ == '__main__':
     unittest.main()
