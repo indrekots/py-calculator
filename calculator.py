ca49d4b27407812e2afcd5a20e546db85706d538
Indrek Ots <ots.indrek@gmail.com>
2015-09-15 19:44:38 +0300

    refactor operator append

--- calculator.py
+++ calculator.py
@@ -12,11 +12,11 @@ class Calculator:
     def calculate(self, input_string):
         output = deque([])
         operators = []
-        elems = self.__find_expression_elements(input_string)
+        elems = self.__find_exp_elems(input_string)
         self.__parse_infix_notation(elems, output, operators)
         return self.__eval_rpn(output)
 
-    def __find_expression_elements(self, input_string):
+    def __find_exp_elems(self, input_string):
         regexp = "\d+|[{}]".format("".join(self.supported_operators.keys()))
         elems = re.findall(regexp, input_string)
         if not elems:
@@ -37,28 +37,37 @@ class Calculator:
         operands = []
         while len(output) > 0:
             token = output.popleft()
-            if isinstance(token, float):
+            if self.__is_operand(token):
                 operands.append(token)
-            else:
-                if token == "+": self.__add(operands)
-                if token == "-": self.__subtract(operands)
-                if token == "*": self.__multiply(operands)
-                if token == "/": self.__divide(operands)
+            else: self.__eval_operator(token, operands)
 
         if len(operands) == 1:
             return operands.pop()
         else: raise RuntimeError("Input has too many values")
 
+    def __is_operand(self, token):
+        return isinstance(token, float)
+
+    def __eval_operator(self, token, operands):
+        if token == "+": self.__add(operands)
+        elif token == "-": self.__subtract(operands)
+        elif token == "*": self.__multiply(operands)
+        elif token == "/": self.__divide(operands)
+
     def __append_operator(self, operators, output, op1):
         while len(operators) > 0:
             op2 = operators[-1]
-            if ((self.__is_left_associative(op1) and self.__op_precedence(op1) <= self.__op_precedence(op2)) or
-                (self.__is_right_associative(op1) and self.__op_precedence(op1) <= self.__op_precedence(op2))):
+            if self.__should_pop_op_off_stack(op1, op2):
                 output.append(operators.pop())
-            else:
-                break
+            else: break
 
         operators.append(op1)
+
+    def __should_pop_op_off_stack(self, op1, op2):
+        return (self.__is_left_associative(op1) and
+                self.__op_precedence(op1) <= self.__op_precedence(op2)) or \
+               (self.__is_right_associative(op1) and
+                self.__op_precedence(op1) < self.__op_precedence(op2))
 
     def __is_left_associative(self, op):
         return self.supported_operators[op]["assoc"] == "left"
